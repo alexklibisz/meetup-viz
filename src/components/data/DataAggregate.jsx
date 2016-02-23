0def337132dfabaecd99b0e25fd51bea6cdaf7bb
Alex Klibisz <aklibisz@gmail.com>
2016-02-23 13:33:35 -0500

    added rsvp counter

--- src/components/data/DataAggregate.jsx
+++ src/components/data/DataAggregate.jsx
@@ -1,17 +1,38 @@
 'use strict';
 import React from 'react';
 import Header from 'components/layout/Header';
+import AltContainer from 'alt-container';
+import MeetupStore from 'stores/MeetupStore';
 
 export const DataAggregate = React.createClass({
 
+  displayName: 'DataAggregate',
+
   render() {
+
+    const rsvpCount = this.props.MeetupStore.rsvpCount;
+
     return (
       <div className='data-aggregate'>
-        Data Aggregate
+        <h4>RSVPs received: {rsvpCount}</h4>
       </div>
     );
   }
 
 });
 
-export default DataAggregate;
+export const SyncComponent = React.createClass({
+
+  displayName: 'SyncComponent',
+
+  render() {
+    return (
+      <AltContainer stores={{MeetupStore}}>
+        <DataAggregate/>
+      </AltContainer>
+    );
+  }
+
+});
+
+export default SyncComponent;
