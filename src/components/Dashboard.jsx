88a4d704f9276f8f5f599b619faffb4911215e72
Alex Klibisz <aklibisz@gmail.com>
2016-04-03 02:00:42 -0400

    adding a simple debug server and react perf in the dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,6 +8,8 @@ import MeetupService from 'services/MeetupService';
 import MeetupActions from 'flux/MeetupActions';
 import MeetupStore from 'flux/MeetupStore';
 import AltContainer from 'alt-container';
+import Perf from 'react-addons-perf';
+import Ax from 'axios';
 
 export const Dashboard = React.createClass({
 
@@ -43,18 +45,30 @@ export const Dashboard = React.createClass({
 
 });
 
+Perf.start();
+
 export const SyncComponent = React.createClass({
 
   displayName: 'DashboardSyncComponent',
 
   componentWillMount() {
-    let count = 0;
-    const countKey = `MV: ${new Date().toLocaleString()}`;
+    let count = 0, sessionId = new Date().getTime();
     const socket = MeetupService.getRSVPSocket();
     socket.onmessage = (data) => {
+      if(count % 10 === 0) {
+        Ax.post('http://localhost:5000/perf', {
+          sessionId,
+          storeState: MeetupStore.getState(),
+          perf: Perf.getLastMeasurements()
+        }).then((res) => console.log(res.data));
+      }
+      count += 1;
       MeetupActions.addRSVP(data);
-      localStorage.setItem(countKey, count++);
     };
+  },
+
+  componentWillUnmount() {
+    Perf.stop();
   },
 
   render() {
