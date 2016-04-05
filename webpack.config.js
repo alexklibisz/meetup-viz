12a6715c0959888a5f5e2af175311e443047cf97
Alex Klibisz <aklibisz@gmail.com>
2016-04-05 09:53:10 -0400

    simplifying webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,54 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var config = {};
-
-// Define entry points with setup for webpack server and react hot reloading.
-config.entry = {
-  main: [
-    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
-    './src/main.js'
-  ]
-};
-
-// Enable source maps for debugging
-config.devtool = 'source-map';
-
-config.output = {
-  path: path.join(__dirname, 'dist'),
-  filename: '[name].js'
-};
-
-config.plugins = [
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'src/index.html'),
-    inject: 'body'
-  })
-];
-
-config.devServer = {
-  contentBase: './dist'
-};
-
-config.resolve = {
-  root: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
-  extensions: ['', '.js', '.jsx']
-};
-
-config.module = {
-  loaders: [{
+module.exports = {
+  // Entry point for js bundles.
+  entry: {
+    main: [
+      'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
+      './src/main.js'
+    ]
+  },
+  // Use source-maps.
+  devtool: 'source-map',
+  // Output to dist/main.js
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  // Plugins for hot-reloading and copying over the index.html file.
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      inject: 'body'
+    })
+  ],
+  // Dev server set up. Serve from dist directory.
+  devServer: {
+    contentBase: './dist'
+  },
+  // How to resolve imports. Use src/ and node_modules/ as roots.
+  resolve: {
+    root: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules')
+    ],
+    extensions: ['', '.js', '.jsx']
+  },
+  // Handle js, jsx, scss files with webpack loaders.
+  module: {
+    loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel-loader'],
-      include: path.join(__dirname, 'src')
-    },
-    {
+      include: path.join(__dirname, 'src'),
+      loaders: ['react-hot', 'babel-loader']
+
+    }, {
       test: /\.scss$/,
       include: path.join(__dirname, 'src'),
       loaders: ['style', 'css?sourceMap!sass?sourceMap'],
-    }
-  ]
+    }]
+  }
 };
-
-module.exports = config;
