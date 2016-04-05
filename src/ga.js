ee061cd1c6ae76b7f1dbffaa60e0246bf20945ef
Alex Klibisz <aklibisz@gmail.com>
2016-04-05 15:06:31 -0400

    ignoring localhost for ga

--- src/ga.js
+++ src/ga.js
@@ -2,5 +2,7 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-57735108-2', 'auto');
-ga('send', 'pageview');
+if(document.location.hostname !== 'localhost') {
+  ga('create', 'UA-57735108-2', 'auto');
+  ga('send', 'pageview');
+}
